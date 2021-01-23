d42f1cc7b06dd536c00d8c5f3fd64a3ff337c49b
Augusto Peres <augustoapg@gmail.com>
2021-01-23 18:50:58 -0500

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,28 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from '../routes';
+import { RouterModule } from '@angular/router';
+import { CreateEventComponent } from './events/create-event.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    EventService
+    EventService,
+    ToastrService
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,6 @@
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from '../common/toastr.service';
 import { EventService } from './shared/event.service';
 
 @Component({
@@ -9,7 +11,7 @@ import { EventService } from './shared/event.service';
     <hr />
     <div class="row">
       <div class="col-lg-4 col-md-6" *ngFor="let event of events">
-        <event-thumbnail [event]="event"></event-thumbnail>
+        <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
       </div>
     </div>
   </div>
@@ -19,11 +21,15 @@ import { EventService } from './shared/event.service';
 export class EventsListComponent implements OnInit {
   events:any[];
 
-  constructor (private eventService : EventService) {
+  constructor (private eventService : EventService, private toastr : ToastrService) {
 
   }
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName);
   }
 }
