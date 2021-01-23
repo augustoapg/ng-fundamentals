ba2a5a1fa647e8ba2a68cfa74f50bfa3c7f1eac5
Augusto Peres <augustoapg@gmail.com>
2021-01-23 17:21:05 -0500

    further implementation including data bindings, service and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
+  ],
+  providers: [
+    EventService
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  templateUrl: './events-app.component.html'
+  template: `
+    <nav-bar></nav-bar>
+    <events-list></events-list>
+  `
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
