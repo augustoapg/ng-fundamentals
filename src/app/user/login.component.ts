5707b004d880b9026d30af11a822eaf5e912fcd9
Augusto Peres <augustoapg@gmail.com>
2021-02-04 18:05:45 -0500

    add profile and login forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,14 @@ import { AuthService } from "./auth.service";
 
 @Component({
   templateUrl: "./login.component.html",
+  styles: [`
+    em { float: right; color: #E05C65; padding-left: 10px; }
+  `]
 })
 export class LoginComponent {
   username
   password
+  mouseoverLogin
 
   constructor(private authService:AuthService, private router:Router) {
 
