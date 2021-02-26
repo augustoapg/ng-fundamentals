a2344c480d27cf82d710d562760782f105fdebe1
Augusto Peres <augustoapg@gmail.com>
2021-02-26 18:29:37 -0500

    add authentication via local server

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,16 +1,23 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 import { IUser } from "./user.model";
 
 @Injectable()
 export class AuthService {
   currentUser:IUser
+
+  constructor(private http:HttpClient) {}
+
   loginUser(userName: string, password: string) {
-    this.currentUser = {
-      id: 1,
-      firstName: "John",
-      lastName: "Doe",
-      userName: userName
-    }
+    const loginInfo = { username: userName, password: password };
+    const options = {headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+    return this.http.post('/api/login', loginInfo, options)
+      .pipe(tap(data => {
+        this.currentUser = <IUser>data['user'];
+      }))
+      .pipe(catchError(err => of(false)));
   }
 
   isAuthenticated() {
--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -12,14 +12,23 @@ export class LoginComponent {
   username
   password
   mouseoverLogin
+  loginInvalid = false;
 
   constructor(private authService:AuthService, private router:Router) {
 
   }
 
   login(formValues) {
-    this.authService.loginUser(formValues.username, formValues.password);
-    this.router.navigate(['events']);
+    this.authService.loginUser(formValues.username, formValues.password).subscribe(
+      resp => {
+        if (!resp) {
+          this.loginInvalid = true;
+        } else {
+          this.loginInvalid = false;
+          this.router.navigate(['events']);
+        }
+      }
+    );
   }
 
   cancel() {
