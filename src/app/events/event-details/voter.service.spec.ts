d465518c7dbd5312ea7944980a9568895c4e0568
Augusto Peres <augustoapg@gmail.com>
2021-03-02 18:26:52 -0500

    add tests for session-list component

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -19,6 +19,24 @@ describe('VoterService', () => {
       voterService.deleteVoter(6, <ISession>session, 'john');
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toBe('joe');
-    })
+    });
+
+    it('should call http.delete with right url', () => {
+      let session = {id: 3, voters: ['john', 'joe']};
+      mockHttp.delete.and.returnValue(of(false));
+
+      voterService.deleteVoter(6, <ISession>session, 'john');
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/6/sessions/3/voters/john');
+    });
+  });
+
+  describe('addVoter', () => {
+    it('should call http.delete with right url', () => {
+      let session = {id: 3, voters: ['john']};
+      mockHttp.post.and.returnValue(of(false));
+
+      voterService.addVoter(6, <ISession>session, 'joe');
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/6/sessions/3/voters/joe', {}, jasmine.any(Object));
+    });
   });
 })
